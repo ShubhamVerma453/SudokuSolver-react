5c653d626e42cea24c454730a68e75c33ff9eba6
ShubhamVerma453 <sv4537373@gmail.com>
2023-03-19 11:37:33 +0530

    initial setup completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App (){
+    
+
+    return <div>
+        <Header />
+            <h1>this is h1</h1>
+        <Footer />
+        </div>
+}
+
+export default App;
